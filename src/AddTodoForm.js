2d5b535701f6e57420110bd3e74e520a24b44ea7
Alisha James <alisha@Alishas-MacBook-Pro.local>
2021-11-01 18:51:11 -0400

    Lesson 1.3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const AddTodoForm = () => (
-        <form>
+function AddTodoForm(props){
+
+    const handleAddTodo = (event) => {
+
+        event.preventDefault();
+
+        const todoTitle = document.querySelector('input[name=title]').value;
+        console.log(todoTitle);
+
+        props.onAddTodo(todoTitle);
+
+        document.querySelector('form').reset();
+    };
+
+    return(
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title </label>
-            <input id="todoTitle" type="text" />
+            <input id="todoTitle" type="text" name="title" />
             <button type="submit">Add</button>
         </form>
-    );
+    );    
+};
 
 export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-const App = () => (
+function App(){
+  const [newTodo, setNewTodo] = React.useState('');
+
+  return(
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList />
     </div>  
   );
+};
 
 export default App;
